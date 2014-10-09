9676fbc8c19f37d4841b715f1c1f3000086ea8e8
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2014-10-09 19:15:06 +0400

    ajax updates fix

--- src/main/java/ru/buls/wicket/FieldsRepeater.java
+++ src/main/java/ru/buls/wicket/FieldsRepeater.java
@@ -2,29 +2,22 @@ package ru.buls.wicket;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.Response;
 import org.apache.wicket.markup.*;
 import org.apache.wicket.markup.html.WebMarkupContainerWithAssociatedMarkup;
 import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.parser.XmlTag;
 import org.apache.wicket.markup.parser.filter.WicketTagIdentifier;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.Serializable;
-
-import static org.apache.wicket.markup.MarkupResourceData.NO_MARKUP_RESOURCE_DATA;
+import java.util.Iterator;
 
 
 /**
  * Created by alexander on 05.10.14.
  */
 public class FieldsRepeater extends MarkupContainer {
-
-    private Logger logger = LoggerFactory.getLogger(FieldsRepeater.class);
 
     static {
         WicketTagIdentifier.registerWellKnownTagName("field");
@@ -41,38 +34,52 @@ public class FieldsRepeater extends MarkupContainer {
 
     @Override
     protected void onRender(final MarkupStream markupStream) {
+        ComponentTag startTag = markupStream.getTag();
+        assert startTag != null;
         int startIndex = markupStream.getCurrentIndex();
+        int endIndex = -1;
+        Iterator<? extends Component> it = iterator();
+        if (it.hasNext()) do {
+            Component child = it.next();
+            if (child == null) {
+                throw new IllegalStateException("the render iterator returned null for a child");
+            }
 
-        for (int i = 0; i < size(); ++i) {
-            markupStream.setCurrentIndex(startIndex);
-            Component component = get(i);
-            component.render(markupStream);
+            ComponentTag newStart = new ComponentTag(startTag.getName(), startTag.getType());
+            newStart.getAttributes().putAll(startTag.getAttributes());
+            newStart.getAttributes().remove("wicket:id");
+            MarkupElement elem = newStart;
+
+            renderTag(elem, child);
+
+            while (!startTag.isOpenClose() && markupStream.hasMore()) {
+                elem = markupStream.next();
+                renderTag(elem, child);
+                if (elem instanceof ComponentTag
+                        && startTag == ((ComponentTag) elem).getOpenTag()) {
+                    endIndex = markupStream.getCurrentIndex();
+                    markupStream.setCurrentIndex(startIndex);
+                    break;
+                }
+            }
+
+        } while (it.hasNext());
+        else markupStream.skipComponent();
+
+        if (endIndex > 0) {
+            markupStream.setCurrentIndex(endIndex);
+            if (markupStream.hasMore()) markupStream.next();
         }
     }
 
-    private void copy(Markup from, Markup to) {
-        for (int i = 0; i < from.size(); ++i) {
-            to.addMarkupElement(from.get(i));
-        }
-    }
-
-    private String getEnclosureId(Component child) {
-        return "enclosureFor" + child.getMarkupId();
-    }
-
-    private Markup renderTag(MarkupElement tag, Component child) {
-        Markup childMarkup;
+    private void renderTag(MarkupElement tag, Component child) {
+        Response response = getResponse();
         if (tag instanceof WicketTag) {
             WicketTag wtag = (WicketTag) tag;
             String name = wtag.getName();
-            if (!wtag.isOpenClose()) {
-                throw new IllegalStateException(tag + " must be closed");
-            }
-            if ("field".equals(name))
-                childMarkup = createChildMarkup(child, wtag);
-            else if ("label".equals(name))
-                childMarkup = getLabelMarkup(child);
-            else throw new IllegalStateException(tag.toString());
+            if (!wtag.isOpenClose()) throw new IllegalStateException(tag + " must be closed");
+            if ("field".equals(name)) renderChild(child, wtag);
+            else if ("label".equals(name)) renderLabel(child, wtag);
         } else if (tag instanceof ComponentTag) {
             ComponentTag cTag = (ComponentTag) tag;
             if ((cTag.isOpen() || cTag.isOpenClose()) && "label".equals(cTag.getName())) {
@@ -81,65 +88,49 @@ public class FieldsRepeater extends MarkupContainer {
                     ComponentTag newTag = new ComponentTag(cTag.getName(), cTag.getType());
                     newTag.getAttributes().putAll(cTag.getAttributes());
                     cTag = newTag;
-                    //cTag.getAttributes().remove("wicket:for");
-                    cTag.getAttributes().put("wicket:for", getChildId(child));
+                    cTag.getAttributes().remove("wicket:for");
+                    cTag.getAttributes().put("for", getChildId(child));
                 } else if (wicketFor != null) {
                     throw new IllegalStateException("incorrect value '" + wicketFor
                             + "' for attribute wicket:field of tag " + cTag);
                 }
             }
-
-            Markup markup = new Markup(NO_MARKUP_RESOURCE_DATA);
-            markup.addMarkupElement(cTag);
-
-            childMarkup = markup;
-        } else {
-            Markup markup = new Markup(NO_MARKUP_RESOURCE_DATA);
-            markup.addMarkupElement(tag);
-            childMarkup = markup;
-            //throw new IllegalStateException(tag.toString());
-        }//response.write(tag.toCharSequence());
-        return childMarkup;
-        //response.write(tag);
+            response.write(cTag);
+        } else response.write(tag.toCharSequence());
     }
 
-
-    private Markup createChildMarkup(Component child, WicketTag wtag) {
+    protected void renderChild(Component child, WicketTag wtag) {
         String tagName = childTagBuilder.getTagName(child);
         ComponentTag open = childTagBuilder.createOpenTag(child, tagName);
 
         String childId = getChildId(child);
         open.setId(childId);
-        open.put("wicket:id", childId);
+        open.getAttributes().put("id", childId);
 
-        //копируем атрибуты викет тега
-        if (wtag != null)
-            open.getAttributes().putAll(wtag.getAttributes());
+        open.getAttributes().putAll(wtag.getAttributes());
 
         ComponentTag close = childTagBuilder.createCloseTag(tagName, open);
-        return createChildMarkup(open, close);
+        MarkupStream markupStream = createChildMarkup(open, close);
+        child.render(markupStream);
     }
 
-    private Markup createChildMarkup(MarkupElement open, MarkupElement close) {
-        Markup markup = new Markup(NO_MARKUP_RESOURCE_DATA);
+    private MarkupStream createChildMarkup(MarkupElement open, MarkupElement close) {
+        Markup markup = new Markup(MarkupResourceData.NO_MARKUP_RESOURCE_DATA);
         if (open instanceof ComponentTag) {
             markup.addMarkupElement(open);
             if (close != null) markup.addMarkupElement(close);
         }
-        return markup;
+        return new MarkupStream(markup);
     }
 
     protected String getChildId(Component child) {
-        return childIdDecorator.decorate(child.getMarkupId());
+        return childIdDecorator.decorate(child.getId());
     }
 
-    private Markup getLabelMarkup(Component child) {
+    protected void renderLabel(Component child, WicketTag wtag) {
         String label = labelDecorator.decorate(getLabel(child));
 
-        Markup markup = new Markup(NO_MARKUP_RESOURCE_DATA);
-        markup.addMarkupElement(new RawMarkup(label));
-
-        return markup;
+        getResponse().write(label);
     }
 
     protected String getLabel(Component child) {
@@ -151,18 +142,6 @@ public class FieldsRepeater extends MarkupContainer {
             label = model != null ? model.toString() : label;
         }
         return label;
-    }
-
-    public FieldsRepeater add(Component child) {
-        Enclosure enclo = new Enclosure(getEnclosureId(child));
-        enclo.add(child);
-        enclo.setOutputMarkupId(true);
-        enclo.setOutputMarkupPlaceholderTag(true);
-        child.setOutputMarkupPlaceholderTag(true);
-        child.setMarkupId(child.getId());
-        super.add(enclo);
-        return this;
-
     }
 
     static class ChildIdDecorator implements Decorator<String> {
@@ -189,9 +168,7 @@ public class FieldsRepeater extends MarkupContainer {
             else if (child instanceof WebMarkupContainerWithAssociatedMarkup || child instanceof FormComponentPanel)
                 tagName = "span";
             else if (child instanceof FieldsRepeater) tagName = "span";
-            else {
-                throw new UnsupportedOperationException("does not support child element " + child);
-            }
+            else throw new UnsupportedOperationException("does not support child element " + child);
             return tagName;
         }
 
@@ -208,103 +185,5 @@ public class FieldsRepeater extends MarkupContainer {
             return open;
         }
 
-    }
-
-    class Enclosure extends MarkupContainer {
-
-        private String generatedMarkup;
-
-        public Enclosure(String id) {
-            super(id, new Model());
-        }
-
-        @Override
-        public MarkupStream getAssociatedMarkupStream(boolean throwException) {
-            Markup markup;
-            if (generatedMarkup == null) {
-                StringBuilder builder = new StringBuilder();
-                markup = generate();
-                for (int i = 0; i < markup.size(); ++i) {
-                    MarkupElement element = markup.get(i);
-                    if (element != null) builder.append(element.toCharSequence());
-                }
-                generatedMarkup = builder.toString();
-            } else try {
-                Markup parse = new MarkupParser(generatedMarkup).parse();
-                copy(parse, markup = new Markup(NO_MARKUP_RESOURCE_DATA));
-            } catch (IOException e) {
-                logger.error("error on parsing generated markup : " + generatedMarkup, e);
-                throw new RuntimeException(e);
-            } catch (ResourceStreamNotFoundException e) {
-                logger.error("error on parsing generated markup : " + generatedMarkup, e);
-                throw new RuntimeException(e);
-            }
-            return new MarkupStream(markup);
-        }
-
-        protected Markup generate() {
-            MarkupStream markupStream = getMarkupStream();
-            Markup markup = new Markup(NO_MARKUP_RESOURCE_DATA);
-
-            int endIndex = -1;
-
-            ComponentTag startTag = markupStream.getTag();
-            assert startTag != null;
-
-            Component child = this.get(0);
-
-            ComponentTag tag = new ComponentTag(startTag.getName(), startTag.getType());
-            tag.putAll(startTag.getAttributes());
-            tag.put("id", this.getMarkupId());
-            markup.addMarkupElement(tag);
-            MarkupElement next;
-            while (null != (next = markupStream.next())) {
-
-                boolean exit = false;
-                if (next instanceof ComponentTag) {
-                    ComponentTag cnext = (ComponentTag) next;
-                    ComponentTag ot = cnext.getOpenTag();
-                    if (ot != null && ot.equals(startTag)) {
-                        exit = true;
-                        endIndex = markupStream.getCurrentIndex();
-                    }
-                }
-
-                copy(renderTag(next, child), markup);
-
-                if (exit) break;
-            }
-
-            if (endIndex > 0) markupStream.setCurrentIndex(endIndex);
-            if (markupStream.hasMore()) markupStream.next();
-            return markup;
-        }
-
-        @Override
-        public boolean hasAssociatedMarkup() {
-            return true;
-        }
-
-        @Override
-        protected void onRender(MarkupStream badStream) {
-            MarkupStream stream = getAssociatedMarkupStream(false);
-            Component child = get(0);
-            boolean childRendered = false;
-            MarkupElement next = stream.get();
-            while (next != null) do {
-                if (next instanceof ComponentTag) {
-                    ComponentTag ctag = (ComponentTag) next;
-                    if (child.getId().equals(ctag.getId())) {
-                        child.render(stream);
-                        childRendered = true;
-                    } else getResponse().write(ctag);
-
-                } else if (next != null) getResponse().write(next.toCharSequence());
-            } while (stream.hasMore() && null != (next = stream.next()));
-
-            if (!childRendered) {
-                throw new IllegalStateException("cannot find tag for child " + child);
-            }
-        }
     }
 }
